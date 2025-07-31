489ec375e8eb2e0cfa3293f73c81580eb84aaf3b
mehadishakil <mehadishakil469@gmail.com>
2025-07-31 13:34:19 +0600

    Initial Commit

---
+++ MacPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacPad
+//
+//  Created by Mehadi Hasan on 31/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacPad/MacPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPadApp.swift
+//  MacPad
+//
+//  Created by Mehadi Hasan on 31/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MacPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
